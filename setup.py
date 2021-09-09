63426e5c9bfbef2f73c11c5574a5fba1f5b2490a
Domhnall Carlin <d.carlin@qub.ac.uk>
2021-09-09 20:00:27 +0000

    Add new file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ckan_uploader',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='domhnall carlin',
+    author_email='',
+    description=''
+)
